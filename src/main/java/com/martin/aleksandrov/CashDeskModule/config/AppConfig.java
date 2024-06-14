f4f950d246c5a2b836fdf1e302ae9f0f6faf19e8
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-06-14 19:17:24 +0300

    Stored Api key in application.properties and use it where is needed

--- src/main/java/com/martin/aleksandrov/CashDeskModule/config/AppConfig.java
+++ src/main/java/com/martin/aleksandrov/CashDeskModule/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.martin.aleksandrov.CashDeskModule.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +11,12 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Value("${api.key}")
+    public String apiKey;
+
+    public String getApiKey() {
+        return apiKey;
     }
 }
