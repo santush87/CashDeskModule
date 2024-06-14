b748e99a9dbb851ef53c018f4671fd1e4e552dfe
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-06-14 12:36:26 +0300

    Added initialization when the program starts for the first time

---
+++ src/main/java/com/martin/aleksandrov/CashDeskModule/CashDeskModuleApplication.java
@@ -0,0 +1,13 @@
+package com.martin.aleksandrov.CashDeskModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashDeskModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashDeskModuleApplication.class, args);
+	}
+
+}
